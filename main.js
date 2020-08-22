cf6046857846b4bd9f1a7a35908bbad80b6d0c55
Pavel.Zagainov <pasha_zigzag@mail.ru>
2020-08-23 02:29:28 +1000

    First game commit

--- main.js
+++ main.js
@@ -1,41 +1,74 @@
-//First task
-const firstRow = 'мама мыла раму';
-const secondRow = 'собака друг человека';
-
-function getRow(firstRow, secondRow) {
-    let firstRowCount = 0,
-        secondRowCount = 0;
-    for(let i = 0; i < firstRow.length; i++) {
-        if(firstRow.charAt(i) == 'а') firstRowCount++;
-    }
-    for(let i = 0; i < secondRow.length; i++) {
-        if(secondRow.charAt(i) == 'а') secondRowCount++;
-    }
-    return firstRowCount > secondRowCount ? firstRow : secondRow;
+const character = {
+    name: 'Pikachu',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-character'),
+    elProgressbar: document.getElementById('progressbar-character'),
+    kickBtn: document.getElementById('btn-kick-character')
 }
 
-console.log(getRow(firstRow, secondRow)); // мама мыла раму
-
-//Second task
-function formattedPhone(phone) {
-    correcrPhone = '';
-    for(let i = 0; i < phone.length; i++) {
-        switch(i) {
-            case 2:
-                correcrPhone += ' (' + phone.charAt(i);
-                break;
-            case 5:
-                correcrPhone += ') ' + phone.charAt(i);
-                break;
-            case 8:
-            case 10:
-                correcrPhone += '-' + phone.charAt(i);
-                break;
-            default:
-                correcrPhone += phone.charAt(i);
-        }
-    }
-    return correcrPhone;
+const enemy = {
+    name: 'Charmander',
+    defaultHP: 100,
+    damageHP: 100,
+    elHP: document.getElementById('health-enemy'),
+    elProgressbar: document.getElementById('progressbar-enemy'),
+    kickBtn: document.getElementById('btn-kick-enemy')
 }
 
-console.log(formattedPhone('+71234567890')); // +7 (123) 456-78-90
+character.kickBtn.addEventListener('click', function() {
+    changeMove(this, enemy)
+    changeHP(random(20), enemy)
+})
+
+enemy.kickBtn.addEventListener('click', function() {
+    changeMove(this, character)
+    changeHP(random(20), character)
+})
+
+function init() {
+    console.log('start game');
+    renderHP(character)
+    renderHP(enemy)
+}
+
+function renderHP(person) {
+    renderHPLife(person)
+    renderProgressbarHP(person)
+}
+
+function renderHPLife(person) {
+    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP
+}
+
+function renderProgressbarHP(person) {
+    person.elProgressbar.style.width = person.damageHP + '%'
+}
+
+function changeHP(count, person) {
+    if(person.damageHP <= count) {
+        person.damageHP = 0
+        endGame(person)
+    } else {
+        person.damageHP -= count
+    }
+    
+    renderHP(person)
+}
+
+function random(num) {
+    return Math.ceil(Math.random() * num)
+}
+
+function changeMove(clickedBtn, person) {
+    clickedBtn.disabled = true
+    person.kickBtn.disabled = false
+}
+
+function endGame(person) {
+    alert('Бедный ' + person.name + ' проиграл!')
+    character.kickBtn.disabled = true
+    enemy.kickBtn.disabled = true
+}
+
+init()
